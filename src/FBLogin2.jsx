ffa15cd0739ecc345519aa77750846776f574111
jasminbegam-A <jasminabdul499@gmail.com>
2025-02-13 16:28:10 +0530

    commit

--- src/FBLogin2.jsx
+++ src/FBLogin2.jsx
@@ -4,6 +4,28 @@ import React, { useState } from "react";
 const FacebookBusinessLogin = () => {
   const [userData, setUserData] = useState(null);
 
+    useEffect(() => {
+      window.fbAsyncInit = function () {
+        FB.init({
+          appId: "2525589477642277",
+          cookie: true,
+          xfbml: true,
+          version: "v22.0",
+        });
+      };
+
+      (function (d, s, id) {
+        let js,
+          fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      })(document, "script", "facebook-jssdk");
+    }, []);
+
+
   const handleLoginClick = () => {
     if (!window.FB) {
       console.error("Facebook SDK not loaded yet!");
