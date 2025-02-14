1f955c1196af80c6a48ab3c0bf5f4e882a036936
jasminbegam-A <jasminabdul499@gmail.com>
2025-02-14 08:59:54 +0530

    commit

--- src/FBLogin2.jsx
+++ src/FBLogin2.jsx
@@ -1,94 +1,132 @@
-//USING FRONTEND ONLY
+// //USING FRONTEND ONLY
 
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
-const FacebookBusinessLogin = () => {
-  const [userData, setUserData] = useState(null);
+// const FacebookBusinessLogin = () => {
+//   const [userData, setUserData] = useState(null);
 
-  useEffect(() => {
-    window.fbAsyncInit = function () {
-      FB.init({
-        appId: "2525589477642277",
-        cookie: true,
-        xfbml: true,
-        version: "v20.0",
-      });
-    };
+//   useEffect(() => {
+//     window.fbAsyncInit = function () {
+//       FB.init({
+//         appId: "2525589477642277",
+//         cookie: true,
+//         xfbml: true,
+//         version: "v20.0",
+//       });
+//     };
 
-    (function (d, s, id) {
-      let js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
-  }, []);
+//     (function (d, s, id) {
+//       let js,
+//         fjs = d.getElementsByTagName(s)[0];
+//       if (d.getElementById(id)) return;
+//       js = d.createElement(s);
+//       js.id = id;
+//       js.src = "https://connect.facebook.net/en_US/sdk.js";
+//       fjs.parentNode.insertBefore(js, fjs);
+//     })(document, "script", "facebook-jssdk");
+//   }, []);
 
-  const handleLoginClick = () => {
-    if (!window.FB) {
-      console.error("Facebook SDK not loaded yet!");
-      return;
-    }
+//   const handleLoginClick = () => {
+//     if (!window.FB) {
+//       console.error("Facebook SDK not loaded yet!");
+//       return;
+//     }
 
-    FB.login(
-      function (response) {
-        console.log("Facebook Login Response:", response);
+//     FB.login(
+//       function (response) {
+//         console.log("Facebook Login Response:", response);
        
-        console.log("AuthResponse", response.authResponse);
-        console.log("AccessToken", response.authResponse.accessToken);
+//         console.log("AuthResponse", response.authResponse);
+//         console.log("AccessToken", response.authResponse.accessToken);
 
-        if (response.authResponse) {
-          const accessToken = response.authResponse.accessToken;
-           console.log(accessToken)
-          FB.api(
-            "/me",
-            { fields: "id,name,email", access_token: accessToken },
-            function (userInfo) {
-              console.log("User Info:", userInfo);
-              setUserData(userInfo);
-            }
-          );
-        } else {
-          console.log("User cancelled login or not authorize")
-        }
-      },
+//         if (response.authResponse) {
+//           const accessToken = response.authResponse.accessToken;
+//            console.log(accessToken)
+//           FB.api(
+//             "/me",
+//             { fields: "id,name,email", access_token: accessToken },
+//             function (userInfo) {
+//               console.log("User Info:", userInfo);
+//               setUserData(userInfo);
+//             }
+//           );
+//         } else {
+//           console.log("User cancelled login or not authorize")
+//         }
+//       },
       
-      {
-        config_id: "1121290476363268",
-        response_type: "code",
-        override_default_response_type: true,
-        scope:"email,public_profile"
-      }
+//       {
+//         config_id: "1121290476363268",
+//         response_type: "code",
+//         override_default_response_type: true,
+//         scope:"email,public_profile"
+//       }
       
       
         
-    );
+//     );
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={handleLoginClick} className="login-button">
+//         Login with Facebook Business
+//       </button>
+
+//       {userData && (
+//         <div>
+//           <h3>User Info:</h3>
+//           <p>
+//             <strong>ID:</strong> {userData.id}
+//           </p>
+//           <p>
+//             <strong>Name:</strong> {userData.name}
+//           </p>
+//           <p>
+//             <strong>Email:</strong> {userData.email || "No Email Available"}
+//           </p>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default FacebookBusinessLogin;
+import React, { useState } from "react";
+import FacebookLogin from "react-facebook-login";
+
+const FacebookAuth = () => {
+  const [userData, setUserData] = useState(null);
+
+  const responseFacebook = (response) => {
+    console.log("Facebook Response:", response);
+    setUserData(response);
   };
 
   return (
     <div>
-      <button onClick={handleLoginClick} className="login-button">
-        Login with Facebook Business
-      </button>
+      <FacebookLogin
+        appId="2525589477642277"
+        autoLoad={false}
+        fields="name,email,picture"
+        callback={responseFacebook}
+        icon="fa-facebook"
+      />
 
       {userData && (
         <div>
           <h3>User Info:</h3>
-          <p>
-            <strong>ID:</strong> {userData.id}
-          </p>
           <p>
             <strong>Name:</strong> {userData.name}
           </p>
           <p>
             <strong>Email:</strong> {userData.email || "No Email Available"}
           </p>
+          <img src={userData.picture?.data?.url} alt="Profile" />
         </div>
       )}
     </div>
   );
 };
 
-export default FacebookBusinessLogin;
+export default FacebookAuth;
